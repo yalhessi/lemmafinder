d8cb83ff3f949653df99ed90d981b4274deda246
Aishwarya Sivaraman <aiishwarya.sivaraman@gmail.com>
2022-08-11 14:23:09 -0400

    collect benchmark using filenames instead of file path

--- benchmark/collect_stats.py
+++ benchmark/collect_stats.py
@@ -120,8 +120,9 @@ def count_file(file, lemma_names, lemma_files_names, lemmas_so_far):
                     helper_indices_len = 0
                     while helper_indices_len < len(helper_indices):
                         if file not in lemma_files_names:
-                            lemma_files_names[file] = []
-                        lemma_files_names[file].append((lemma_name,line_number,helper_indices[helper_indices_len][1]))
+                            file_name = os.path.basename(file)
+                            lemma_files_names[file_name] = []
+                        lemma_files_names[file_name].append((lemma_name,line_number,helper_indices[helper_indices_len][1]))
                         count_helper+=1
                         helper_indices_len += 1                   
                         # start_proof = False
