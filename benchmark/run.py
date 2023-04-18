585bd61bb161eee7a49d9de5a844707412cbb8af
Yousef Alhessi <yalhessi@gmail.com>
2023-04-18 11:28:28 -0700

    Link to correct quickchick tmp dir

--- benchmark/run.py
+++ benchmark/run.py
@@ -86,7 +86,7 @@ def get_stuck_state(fname):
     return ""
 
 def clean_quickchick_tmp_dir():
-    shutil.rmtree('dir_path')
+    shutil.rmtree('/tmp/QuickChick')
 
 def run(source_folder, helper_lemma_dict, log_directory, all_lemmas_from_file, example_dir, debug, run_till_cat_1, no_resume, synthesizer, lfind_decl):
     counter = 0
